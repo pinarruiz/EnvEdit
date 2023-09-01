dcfe6d7c38d828d45ca930cdcc00c2bda70ccbc5
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2023-09-01 11:28:50 +0200

    Add automatic login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { SessionContextValue, signIn, useSession } from "next-auth/react";
 import BasicLayout from "@/components/layouts/basic";
 
 export default function Home() {
+  const { data: session, status } = useSession();
+  const authState = React.useState<SessionContextValue["status"]>(status);
+  const userEmailState = React.useState<string>("");
+
+  React.useEffect(() => {
+    if (status === "unauthenticated") {
+      void signIn("gitlab");
+    }
+    authState[1](status);
+    if (session?.user?.email) {
+      userEmailState[1](session.user.email);
+    }
+  }, [status, authState[1], session]);
+
   return <BasicLayout></BasicLayout>;
 }
