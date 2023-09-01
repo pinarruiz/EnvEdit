848433179e84d86e5a08fe7131f4005ad05b88a4
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2023-09-01 10:18:16 +0200

    Add theme provider and theme switcher

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
-import React from "react";
 import "@/styles/globals.css";
+import React from "react";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ThemeProvider } from "@/components/theme/provider";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Component {...pageProps} />
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} position={"bottom-right"} />
     </QueryClientProvider>
   );
