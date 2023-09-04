1defa3d17398cece4d8b76097f5404340e44625e
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2023-09-04 08:57:51 +0200

    Remove padding

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Menu() {
   return (
     <div className="pt-2">
-      <div className="container bg-background p-2 py-3 shadow-sm flex border rounded-lg">
+      <div className="container bg-background p-2 shadow-sm flex border rounded-lg">
         <Button className="text-lg font-semibold" variant={"ghost"} asChild>
           <Link href="/">Home</Link>
         </Button>
