3e34db93e2ac248ee006797d9574011ecd53b560
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2023-09-04 17:00:57 +0200

    Sttart form

--- src/components/variableform/index.tsx
+++ src/components/variableform/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { VariableFormProps } from "@/types/variableform";
 import {
   Dialog,
   DialogContent,
@@ -7,21 +6,37 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  DialogDescription,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Trash } from "lucide-react";
+import { VariableFormProps } from "@/types/variableform";
 
 export default function VariableForm(props: VariableFormProps) {
+  const [environmentValues, setEnvironmentValues] = React.useState(
+    props.variable,
+  );
+
   return (
     <Dialog>
       <DialogTrigger asChild>{props.children}</DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent
+        className="transition-[padding] sm:p-8 px-6 rounded-md overflow-y-auto max-h-[90%] max-w-7xl md:w-[90%] w-[95%]"
+        onOpenAutoFocus={(e) => e.preventDefault()}
+      >
         <DialogHeader>
           <DialogTitle>Edit {props.variable_name}</DialogTitle>
+          <DialogDescription className="opacity-40">
+            Found {Object.keys(environmentValues).length} environments.
+          </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <VariableFormBody
             variable={props.variable}
             variable_name={props.variable_name}
+            environmentValues={environmentValues}
+            setEnvironmentValues={setEnvironmentValues}
           />
         </div>
         <DialogFooter>
@@ -32,6 +47,58 @@ export default function VariableForm(props: VariableFormProps) {
   );
 }
 
-function VariableFormBody(props: VariableFormProps) {
-  return <div>{props.variable_name}</div>;
+function VariableFormBody(
+  props: VariableFormProps & {
+    environmentValues: VariableFormProps["variable"];
+    setEnvironmentValues: React.Dispatch<
+      React.SetStateAction<VariableFormProps["variable"]>
+    >;
+  },
+) {
+  const [anyMod, setAnyMod] = React.useState(false);
+  const envs = Object.keys(props.environmentValues);
+
+  !anyMod &&
+    Object.keys(props.variable).forEach((key) => {
+      if (!envs.includes(key)) {
+        props.setEnvironmentValues((prev) => {
+          return { ...prev, ...{ [key]: props.variable[key] } };
+        });
+      }
+    });
+
+  return (
+    <form className="flex flex-col gap-4">
+      <datalist id={`envs${props.variable_name}`}>
+        {envs.map((env) => (
+          <option key={`evso${props.variable_name}${env}`} value={env} />
+        ))}
+      </datalist>
+      {envs.map((env) => (
+        <div key={`${env}${props.environmentValues[env]}`} className="flex">
+          <Input
+            defaultValue={env}
+            className="w-1/3 mr-7"
+            list={`envs${props.variable_name}`}
+          />
+          <Input defaultValue={props.environmentValues[env]} className="mr-7" />
+          <Button
+            id={env}
+            variant="destructive"
+            type="button"
+            size="icon"
+            className="w-10 h-10 flex-shrink-0"
+            onClick={(e) => {
+              e.preventDefault();
+              setAnyMod(true);
+              delete props.environmentValues[e.currentTarget.id];
+              props.setEnvironmentValues({ ...props.environmentValues });
+            }}
+          >
+            <Trash />
+          </Button>
+        </div>
+      ))}
+    </form>
+  );
 }
