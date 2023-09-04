852a230c702dd6e50b845cc265043a9a517a3283
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2023-09-04 12:36:09 +0200

    Add envs

--- src/components/variablecard/index.tsx
+++ src/components/variablecard/index.tsx
@@ -7,7 +7,7 @@ export default function VariableCard(props: VariableCardProps) {
   return (
     <div
       className={cn(
-        "rounded-md border py-3 px-4 flex transition h-full",
+        "rounded-md border py-3 px-4 flex flex-col sm:flex-row transition h-full",
         props.loading
           ? "hover:cursor-progress"
           : "hover:bg-accent hover:cursor-pointer",
@@ -15,12 +15,19 @@ export default function VariableCard(props: VariableCardProps) {
     >
       {props.loading ? (
         <>
-          <Skeleton className="h-6 w-96" />
-          <Skeleton className="h-6 w-40 ml-auto" />
+          <div className="pb-5">
+            <Skeleton className="h-6 w-96 m-auto sm:m-0 sm:pb-0" />
+          </div>
+          <Skeleton className="h-6 w-40 m-auto sm:m-0 sm:ml-auto" />
         </>
       ) : (
         <>
-          <p className="font-bold">{props.variable_name}</p>
+          <p className="font-bold m-auto sm:m-0 pb-5 sm:pb-0">
+            {props.variable_name}
+          </p>
+          <p className="opacity-40 ml-auto sm:w-2/5 overflow-y-scroll">
+            {Object.keys(props.variable).join(", ")}
+          </p>
         </>
       )}
     </div>
