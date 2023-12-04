d2d2d032662ae3909125552e889b53b40fea5759
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2023-12-04 16:28:52 +0100

    Move components out of directories

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 import { ThemeSwitcher } from "@/components/theme/switcher";
-import { Button } from "../ui/button";
 import Link from "next/link";
 
 export default function Menu() {
