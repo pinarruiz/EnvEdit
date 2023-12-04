3e0fe16c2fa59a78f3c5a957c564998ebfa27c9f
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2023-12-04 17:03:22 +0100

    Change title on editing project

--- src/pages/project/[projectid].tsx
+++ src/pages/project/[projectid].tsx
@@ -1,14 +1,30 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
 import BasicLayout from "@/components/layouts/basic";
 import VariableProcessor from "@/components/variableprocessor";
-import { useRouter } from "next/router";
+import { UserContext } from "@/components/contexts/user";
+import { UserContextProviderType } from "@/types/usercontext";
+import { getProject } from "@/lib/gitlab/projects";
+import { DEFAULT_TITLE } from "@/lib/appEnv";
 
 export default function Project() {
   const router = useRouter();
+  const { userData } = React.useContext(UserContext) as UserContextProviderType;
   const projectId = parseInt(router.query.projectid as string);
 
+  const { data: projectData, status } = useQuery({
+    queryKey: ["projectData", userData.accessToken, projectId],
+    queryFn: () => getProject(userData.accessToken as string, projectId),
+    enabled: userData !== undefined && projectId !== undefined,
+  });
+
   return (
-    <BasicLayout>
+    <BasicLayout
+      title={
+        (status === "success" ? projectData.name + " - " : "") + DEFAULT_TITLE
+      }
+    >
       <VariableProcessor
         projectId={projectId}
         loading={projectId === undefined}
