6167be2fed71446c6d49517690ad0e16fbf40e2a
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2023-12-05 10:07:50 +0100

    Add file upload input

--- src/components/uploadenv/dialog.tsx
+++ src/components/uploadenv/dialog.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
   DialogHeader,
@@ -9,6 +10,8 @@ import {
   DialogTitle,
   DialogDescription,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { X } from "lucide-react";
 
 type UploadEnvDialogProps = {
   children?: React.ReactNode;
@@ -16,9 +19,19 @@ type UploadEnvDialogProps = {
 
 export default function UploadEnvDialog(props: UploadEnvDialogProps) {
   const [openedDialog, setOpenedDialog] = React.useState(false);
+  const [fileUploaded, setFileUploaded] = React.useState(false);
+  const fileInputRef = React.useRef<HTMLInputElement>(null);
+
+  async function handleDialogOpenClose(event: boolean) {
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+    setFileUploaded(false);
+    setOpenedDialog(event);
+  }
 
   return (
-    <Dialog open={openedDialog} onOpenChange={setOpenedDialog}>
+    <Dialog open={openedDialog} onOpenChange={handleDialogOpenClose}>
       <DialogTrigger asChild>{props.children}</DialogTrigger>
       <DialogContent onOpenAutoFocus={(e) => e.preventDefault()}>
         <DialogHeader>
@@ -28,13 +41,46 @@ export default function UploadEnvDialog(props: UploadEnvDialogProps) {
           </DialogDescription>
         </DialogHeader>
         <form className="flex flex-col gap-4">
-          <div className="grid gap-4 py-4"></div>
+          <div className="grid gap-4 py-4">
+            <div
+              className={cn(
+                "duration-300 transition-[gap] flex",
+                fileUploaded ? "gap-3" : "",
+              )}
+            >
+              <Input
+                type="file"
+                ref={fileInputRef}
+                onChange={(e) =>
+                  setFileUploaded(e.currentTarget.files?.length !== 0)
+                }
+              />
+              <Button
+                size="icon"
+                variant="destructive"
+                disabled={!fileUploaded}
+                onClick={(e) => {
+                  e.preventDefault();
+                  if (fileInputRef.current) {
+                    setFileUploaded(false);
+                    fileInputRef.current.value = "";
+                  }
+                }}
+                className={cn(
+                  "transition-all duration-300 group/removebutton h-10",
+                  fileUploaded ? "w-10" : "w-0",
+                )}
+              >
+                <X className="duration-150 rotate-0 group-hover/removebutton:rotate-90" />
+              </Button>
+            </div>
+          </div>
           <DialogFooter>
             <Button
               variant="secondary"
               onClick={(e) => {
                 e.preventDefault();
-                setOpenedDialog(false);
+                handleDialogOpenClose(false);
               }}
             >
               Cancel
