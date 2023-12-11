58f5695c5486d28eeb0f39270ce92a7070fe1c3f
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2023-12-11 13:43:00 +0100

    Move envselector

--- src/components/uploadenv/dialog.tsx
+++ src/components/uploadenv/dialog.tsx
@@ -13,7 +13,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { X } from "lucide-react";
 import { ProjectVariableSchema } from "@gitbeaker/rest";
-import UploadEnvSelector from "@/components/uploadenv/envselector";
+import variablesToScopes from "@/lib/variablesToScopes";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 type UploadEnvDialogProps = {
   children?: React.ReactNode;
@@ -24,6 +25,17 @@ export default function UploadEnvDialog(props: UploadEnvDialogProps) {
   const [openedDialog, setOpenedDialog] = React.useState(false);
   const [fileUploaded, setFileUploaded] = React.useState(false);
   const fileInputRef = React.useRef<HTMLInputElement>(null);
+
+  const envScopes = variablesToScopes(props.variables);
+  const [searchScope, setSearchScope] = React.useState(
+    envScopes.length === 1 ? envScopes[0] : "",
+  );
+
+  const filteredScopes = envScopes.filter(
+    (envScope) =>
+      envScope.toLowerCase().includes(searchScope.toLowerCase()) ||
+      searchScope === "",
+  );
 
   async function handleDialogOpenClose(event: boolean) {
     if (fileInputRef.current) {
@@ -78,10 +90,46 @@ export default function UploadEnvDialog(props: UploadEnvDialogProps) {
                 <X className="duration-150 rotate-0 group-hover/removebutton:rotate-90" />
               </Button>
             </div>
-            <UploadEnvSelector
-              variables={props.variables}
-              enabled={fileUploaded}
-            />
+            <div
+              className={cn(
+                "duration-300 flex flex-col gap-2",
+                fileUploaded ? "h-52" : "h-0 overflow-hidden",
+              )}
+            >
+              <div className="flex">
+                <Input
+                  onChange={(e) => setSearchScope(e.currentTarget.value)}
+                  value={searchScope}
+                  placeholder="Search for environment scope..."
+                />
+              </div>
+              <ScrollArea
+                className={cn(
+                  "duration-300 overflow-auto px-3 py-2 border rounded-md",
+                  (searchScope !== "" && filteredScopes.length < 1) ||
+                    (filteredScopes.length === 1 &&
+                      filteredScopes[0] === searchScope)
+                    ? "h-0 p-0 border-0"
+                    : "",
+                )}
+              >
+                {envScopes.map((envScope) => (
+                  <p
+                    className={cn(
+                      "duration-300 overflow-hidden hover:bg-secondary/80 hover:cursor-pointer rounded-md",
+                      filteredScopes.length <= 1 ? "mb-0" : "mb-1 last:mb-0",
+                      filteredScopes.includes(envScope) || searchScope === ""
+                        ? "h-8 px-2 py-1"
+                        : "h-0 mb-0",
+                    )}
+                    onClick={() => setSearchScope(envScope)}
+                    key={envScope}
+                  >
+                    {envScope}
+                  </p>
+                ))}
+              </ScrollArea>
+            </div>
           </div>
           <DialogFooter>
             <Button
