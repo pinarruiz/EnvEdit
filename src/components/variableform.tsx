1a5d7231d181ad892a5d9db50085635822cc6565
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2024-05-06 00:03:53 +0200

    Start variable env selection

--- src/components/variableform.tsx
+++ src/components/variableform.tsx
@@ -9,9 +9,35 @@ import {
   DialogDescription,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "@/components/ui/accordion";
 import { VariableFormProps } from "@/types/variableform";
 
 export default function VariableForm(props: VariableFormProps) {
+  const variablePool = Object.keys(props.variable)
+    .map((envName) => {
+      return { environment_scope: envName, value: props.variable[envName] };
+    })
+    .reduce(
+      (
+        group: Record<
+          VariableFormProps["variable"]["value"],
+          VariableFormProps["variable"]["environment_scope"][]
+        >,
+        envVar,
+      ) => {
+        const { value } = envVar;
+        group[value] = group[value] ?? [];
+        group[value].push(envVar.environment_scope);
+        return group;
+      },
+      {},
+    );
+
   return (
     <Dialog>
       <DialogTrigger asChild>{props.children}</DialogTrigger>
@@ -26,7 +52,44 @@ export default function VariableForm(props: VariableFormProps) {
           </DialogDescription>
         </DialogHeader>
         <form className="flex flex-col gap-4">
-          <div className="grid gap-4 py-4"></div>
+          <Accordion type="multiple" className="w-full">
+            {Object.keys(variablePool).map((envValue) => (
+              <AccordionItem value={envValue} key={envValue}>
+                <AccordionTrigger>{envValue}</AccordionTrigger>
+                <AccordionContent className="">
+                  {variablePool[envValue].map((envName) => (
+                    <Button
+                      key={envName}
+                      variant="outline"
+                      className="duration-300 transition-[opacity] bg-accent opacity-75 hover:opacity-100 m-1 first:ml-0 last:mr-0"
+                      onClick={(event) => {
+                        event.preventDefault();
+                      }}
+                    >
+                      {envName}
+                    </Button>
+                  ))}
+                  {Object.keys(props.variable)
+                    .filter(
+                      (envVar) =>
+                        !Object.values(variablePool[envValue]).includes(envVar),
+                    )
+                    .map((envName) => (
+                      <Button
+                        key={envName}
+                        variant="outline"
+                        className="m-1 first:ml-0 last:mr-0"
+                        onClick={(event) => {
+                          event.preventDefault();
+                        }}
+                      >
+                        {envName}
+                      </Button>
+                    ))}
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
           <DialogFooter>
             <Button
               type="submit"
