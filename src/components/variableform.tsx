0c3acee184ba6498ce285802175e73f80acb951a
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2024-05-05 20:52:02 +0200

    Remove form dependencies

--- src/components/variableform.tsx
+++ src/components/variableform.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import * as Yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm, useFieldArray } from "react-hook-form";
 import {
   Dialog,
   DialogContent,
@@ -15,48 +12,6 @@ import { Button } from "@/components/ui/button";
 import { VariableFormProps } from "@/types/variableform";
 
 export default function VariableForm(props: VariableFormProps) {
-  const formSchema = Yup.object().shape({
-    environments: Yup.array().of(
-      Yup.object().shape({
-        environment_scope: Yup.string().required("Value is mendatory"),
-        value: Yup.string().required("Value is mendatory"),
-      }),
-    ),
-  });
-
-  const optionsDf = { resolver: yupResolver(formSchema) };
-  const {
-    control,
-    // formState,
-    // handleSubmit,
-    // register,
-    watch,
-    // reset
-  } = useForm(optionsDf);
-
-  // const { errors } = formState;
-  const {
-    // fields,
-    append,
-    // remove
-  } = useFieldArray({
-    name: "environments",
-    control,
-  });
-
-  const environments = watch("environments");
-
-  Object.keys(props.variable).forEach((key) => {
-    if (
-      environments?.filter((env) => env.environment_scope === key).length === 0
-    ) {
-      append({
-        environment_scope: key,
-        value: props.variable[key],
-      });
-    }
-  });
-
   return (
     <Dialog>
       <DialogTrigger asChild>{props.children}</DialogTrigger>
