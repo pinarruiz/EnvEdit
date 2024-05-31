738a2b3a575dc6a2a2f7a9cb82313fbb887da2d3
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2024-05-31 12:28:11 +0200

    Showing only projects with mantainer permissions at least

--- src/lib/gitlab/projects.ts
+++ src/lib/gitlab/projects.ts
@@ -1,14 +1,19 @@
-import { SimpleProjectSchema } from "@gitbeaker/rest";
+import { AccessLevel, SimpleProjectSchema } from "@gitbeaker/rest";
 import { GITLAB_PER_PAGE } from "@/lib/appEnv";
 import { getApiObject } from "@/lib/gitlab/utils";
 
-export async function queryProjects(oauthToken: string, page: number = 1) {
+export async function queryProjects(
+  oauthToken: string,
+  page: number = 1,
+  minAccessLevel: number = AccessLevel.MAINTAINER,
+) {
   return await getApiObject(oauthToken).Projects.all({
     pagination: "offset",
     showExpanded: true,
     page: page,
     simple: true,
     perPage: GITLAB_PER_PAGE,
+    minAccessLevel: minAccessLevel,
   });
 }
 
