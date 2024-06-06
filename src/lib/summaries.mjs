58d3f8319fa8f033c77d60750d3b133db377dc3b
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2024-06-06 10:30:53 +0200

    Add build summary

---
+++ src/lib/summaries.mjs
@@ -0,0 +1,32 @@
+import boxen from "boxen";
+import appEnv from "./appEnv.js";
+
+const boxenGenericOptions = {
+  titleAlignment: "center",
+  padding: 1,
+  textAlignment: "center",
+  borderColor: "green",
+  float: "center",
+};
+
+const buildMessage = [
+  `Gitlab Domain: ${appEnv.GITLAB_DOMAIN}`,
+  `Domain name: ${appEnv.DOMAIN_NAME}`,
+];
+
+const boxes = {
+  build: {
+    message: buildMessage.join("\n"),
+    boxenOptions: {
+      title: "Build Info",
+    },
+  },
+};
+
+const argv = process.argv.slice(2);
+if (argv.length == 1 && Object.keys(boxes).includes(argv[0])) {
+  const boxInfo = boxes[argv[0]];
+  console.log(
+    boxen(boxInfo.message, { ...boxenGenericOptions, ...boxInfo.boxenOptions }),
+  );
+}
