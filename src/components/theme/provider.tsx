fcf0d5d20e21d66107e816df86c47999e98c7333
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2024-11-04 06:08:41 +0000

    fix(deps): update dependency next-themes to v0.4.1

--- src/components/theme/provider.tsx
+++ src/components/theme/provider.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+type ThemeProviderProps = Parameters<typeof NextThemesProvider>[0];
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
