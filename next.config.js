6643c363a4cab9af80b72ec159ca0421fcef8042
Alejandro Pinar Ruiz <alejandropinarruiz@gmail.com>
2025-04-08 19:12:15 +0200

    Fix leaking env vars

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 
-const appEnv = require("./src/lib/appEnv");
+const {
+  DOMAIN_NAME,
+  NEXTAUTH_URL,
+  GITLAB_DOMAIN,
+  GITLAB_PER_PAGE,
+  DEFAULT_TITLE,
+} = require("./src/lib/appEnv");
 
 const nextConfig = {
   reactStrictMode: true,
-  env: appEnv,
+  env: {
+    DOMAIN_NAME,
+    NEXTAUTH_URL,
+    GITLAB_DOMAIN,
+    GITLAB_PER_PAGE,
+    DEFAULT_TITLE,
+  },
 };
 
 module.exports = nextConfig;
